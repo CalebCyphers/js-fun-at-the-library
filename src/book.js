c79c4012a44fa321244a4023693e63d3d86fd8bb
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2022-01-10 01:13:01 -0700

    Make all tests in booktest pass

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  class Character {
+    constructor(name, age, pronouns) {
+      this.name = name
+      this.age = age
+      this.pronouns = pronouns
+    }
+  }
+  return new Character(name, age, pronouns)
+}
+
+function saveReview(review, reviews) {
+  return reviews.includes(review) ? reviews : reviews.push(review)
+}
+
+function calculatePageCount(bookTitle) {
+  let letters = bookTitle.split("")
+  return letters.length * 20
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  class Book {
+    constructor(title, mainCharacter, pageCount, genre) {
+      this.title = title
+      this.mainCharacter = mainCharacter
+      this.pageCount = pageCount
+      this.genre = genre
+    }
+  }
+  return new Book(bookTitle, bookCharacter, calculatePageCount(bookTitle), genre)
+}
+
+function editBook(book) {
+  book.pageCount = Math.floor(book.pageCount * .75)
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
